42b4f24dc7b8d25bddf6865bbe5e01d0f247ce83
sonyrahul520 <sonyasokan313@gmail.com>
2024-04-20 15:25:41 +0100

    Add project files.

---
+++ WebsiteProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
